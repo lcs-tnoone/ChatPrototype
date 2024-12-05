b8d19628e522cf226872cd8336233db2b3aa1d19
Thomas Noone <tnoone@lcs.on.ca>
2024-12-05 11:40:54 -0500

    Created a Vstack on top of a Hstack, made a would you rather text ui

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:50){
+            Text("Would You Rather")
+                .padding()
+                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
+                .shadow(radius: 25)
+            HStack(spacing: 25){
+                Text("Be able to Fly!")
+                    .padding()
+                    .background(Color.red, in: RoundedRectangle(cornerRadius: 8))
+                    .shadow(radius: 20)
+                Text("Or")
+                    .padding()
+                    .background(Color.yellow, in: RoundedRectangle(cornerRadius: 8))
+                    .shadow(radius: 20)
+                Text("Be able to read minds")
+                    .padding()
+                    .background(Color.green, in: RoundedRectangle(cornerRadius: 8))
+                    .shadow(radius: 10)
+            }
         }
         .padding()
     }
